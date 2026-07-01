4cc9c8371ba23af65e5ff1e03fbc20d81256a522
theraven998 <sergio.ramirez06@uptc.edu.co>
2026-07-01 09:40:57 -0500

    chore: actualiza url.json y vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        allowedHosts: ["appear-compressed-impressed-millennium.trycloudflare.com"],
+        allowedHosts: ["meet-florida-routers-coverage.trycloudflare.com"],
     },
     plugins: [react()],
 });
